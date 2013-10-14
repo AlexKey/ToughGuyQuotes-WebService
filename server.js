543f29df097f275b76155e1c54659bf39a0670ff
Alex Key <alexander.key@gmail.com>
2013-10-14 12:02:35 -0400

    http get using nodejs streams to get qoutes and write to the browser

--- server.js
+++ server.js
@@ -1,5 +1,41 @@
 var http = require('http');
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
+
+function getQuotes(firstName, lastName, count, callback) {
+    var url = "http://api.icndb.com/jokes/random/" + count + "?firstName=" + firstName + "&lastName=" + lastName;
+
+    
+    var responeCallBack = function(response) {
+        var str = '';
+
+        //turn the stream into flow mode
+        response.on('data', function(chunk) {
+            str += chunk;
+        });
+        
+        
+        //when all the data is read
+        response.on('end', function() {
+           callback(str); 
+        });
+    
+    }
+    
+    http.get(url,responeCallBack).end();
+}
+
+
+
+http.createServer(function(req, res) {
+    res.writeHead(200, {
+        'Content-Type': 'text/plain'
+    });
+    
+
+    getQuotes("Alex", "Key", 10, function(quotes) {
+    
+        res.end(quotes);
+    });
+    
+
+
 }).listen(process.env.PORT, process.env.IP);
